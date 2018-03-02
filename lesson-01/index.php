26ea3cc98b6a217c933bac72923f8f539b4917e8
gb-education <systempay@mail.ru>
2018-03-02 17:47:48 +0300

    GB Education PHP Lesson 01

---
+++ lesson-01/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$title = 'Интернет-магазин QuadCopter Pro - Главная';
+$h1 = 'Самый большой выбор квадрокоптеров в QuadCopter Pro';
+$year = date('Y');
+
+?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+</head>
+<body>
+	<ul>
+		<li>Главная</li>
+		<li><a href="#" title="">Каталог</a></li>
+		<li><a href="#" title="">Контакты</a></li>
+	</ul>
+	<hr>
+    <h1><?php echo $h1; ?></h1>
+    <p>Привет! Спасибо, что посетил наш сайт. У нас самый большой выбор мультикоптеров на любой вкус, цвет и кошелек.</p>
+	<hr>
+    <p><?php echo $year; ?> &copy; Все права защищены.</p>
+</body>
+</html>
