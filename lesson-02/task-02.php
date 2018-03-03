f1f74d67cab60c77f9c16d4c26a04c079cce81e4
gb-education <systempay@mail.ru>
2018-03-03 14:02:20 +0300

    GB Education PHP HomeWork 02 - v 1.1

--- lesson-02/task-02.php
+++ lesson-02/task-02.php
@@ -17,23 +17,26 @@
 
 $a = $_GET[a];
 
-switch ($a) {
-	case 0: echo $a++." ";
-	case 1: echo $a++." ";
-	case 2: echo $a++." ";
-	case 3: echo $a++." ";
-	case 4: echo $a++." ";
-	case 5: echo $a++." ";
-	case 6: echo $a++." ";
-	case 7: echo $a++." ";
-	case 8: echo $a++." ";
-	case 9: echo $a++." ";
-	case 10: echo $a++." ";
-	case 11: echo $a++." ";
-	case 12: echo $a++." ";
-	case 13: echo $a++." ";
-	case 14: echo $a++." ";
-	case 15: echo $a++." ";
+if (!empty($a)) {
+	switch ($a) {
+		case 0: echo $a++." ";
+		case 1: echo $a++." ";
+		case 2: echo $a++." ";
+		case 3: echo $a++." ";
+		case 4: echo $a++." ";
+		case 5: echo $a++." ";
+		case 6: echo $a++." ";
+		case 7: echo $a++." ";
+		case 8: echo $a++." ";
+		case 9: echo $a++." ";
+		case 10: echo $a++." ";
+		case 11: echo $a++." ";
+		case 12: echo $a++." ";
+		case 13: echo $a++." ";
+		case 14: echo $a++." ";
+		case 15: echo $a++." ";
+		default: echo "Заданное число не попадает в промежуток от 0 до 15";
+	}
 }
 /* Не очень понятная задача и суть упражнения, но цель достигнута. */
 
